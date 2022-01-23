28d169c1f2de3198fd862b504c85715415e16218
Platelets <98194172+EvolutionLab@users.noreply.github.com>
2022-01-23 15:02:06 +0800

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,7 +1,12 @@
 
+
 import pyrosim.pyrosim as pyrosim
 
+
 pyrosim.Start_SDF("box.sdf")
+
+#size: length, width, and height.
 pyrosim.Send_Cube(name="Box1", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box2", pos=[1,0,1.5] , size=[1,1,1])
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,11 +1,15 @@
 
 #adding my first pyfile
 #
-
+import pybullet_data
 import pybullet as p
 import time
 
+
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
 
