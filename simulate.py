323940086c4446384c6c3f7e0b89f1c8e0bd8e60
EvolutionLab <leiming@kmmu.com.cn>
2022-01-25 10:07:46 +0800

    happy

--- simulate.py
+++ simulate.py
@@ -4,18 +4,18 @@
 import pybullet_data
 import pybullet as p
 import time
-
-
 physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 p.setGravity(0,0,-9.8)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 planeId = p.loadURDF("plane.urdf")
 #p.loadSDF("box.sdf")
 p.loadSDF("world.sdf")
 
-print("Start : %s" % time.ctime())
-time.sleep(30)
-print("End : %s" % time.ctime())
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
 
 
 p.disconnect()
