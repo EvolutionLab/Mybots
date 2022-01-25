3585a5f9d6ed3f3bbefdfa8955c574ec4fb2fd39
Platelets <98194172+EvolutionLab@users.noreply.github.com>
2022-01-25 11:13:45 +0800

    Add files via upload

--- simulate.py
+++ simulate.py
@@ -4,18 +4,37 @@
 import pybullet_data
 import pybullet as p
 import time
-
-
 physicsClient = p.connect(p.GUI)
+#打开或关闭GUI控件，“0”等于关闭
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
+
+#该语句的作用是禁用tinyrenderer，也就是不让CPU上的集成显卡来参与渲染工作
+p.configureDebugVisualizer(p.COV_ENABLE_TINY_RENDERER, 0)
+
+
+#设置重力，当设置成+10的时候，物体会飘走
+#设置重力，当设置成-10的时候，物体会落在地上
+p.setGravity(0,0,-10)
+#添加资源路径
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-#p.loadSDF("box.sdf")
+
+#加载URDF模型，此处是加载蓝白相间的陆地
+#改变环境的路径C:\Users\win10\anaconda3\Lib\site-packages\pybullet_data
+planeId = p.loadURDF("plane100.urdf")
 p.loadSDF("world.sdf")
 
-print("Start : %s" % time.ctime())
-time.sleep(30)
-print("End : %s" % time.ctime())
+p.setRealTimeSimulation(1)
+while True:
+    pass
+
+# 开始一千次迭代，也就是一千次交互，每次交互后停顿1/240
+# 注释代码和取消注释代码的快捷键都一样
+# ctrl + /
+
+# for i in range(1000):
+#     p.stepSimulation()
+#     time.sleep(1 / 240)
+
 
 
 p.disconnect()
